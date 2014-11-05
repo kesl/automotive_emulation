5891b5bed90ed60427f5e30b875c8e9a33aec9d3
mijong <emarnsndiej@naver.com>
2014-11-05 08:00:42 +0000

    add send handle data to other rasp to use can protocol.

--- ecu/common/cansendrec/send-can.c
+++ ecu/common/cansendrec/send-can.c
@@ -3,11 +3,51 @@
 
 #define MAXLINE 256
 
+int Get_wheel_value(void)
+{
+    unsigned int wheel=0;
+    int i=0;
+    FILE *f;
+    char Line[256];
+    unsigned char value[3];
+    f=fopen("save.txt","r");
+    //fgets(Line,256,f);
+    unsigned char test[100] ;
+  
+    fgets(test,100,f);
+    //printf("%s\n",test);
+   
+    if(test[0]=='a')
+    {
+
+        if(test[6]=='.'){wheel=(test[5]-48);}
+        else if(test[7]=='.'){wheel=(test[5]-48)*10+(test[6]-48);}
+        else if(test[8]=='.'){wheel=(test[5]-48)*100+(test[6]-48)*10+(test[7]-48);}
+        else ;
+
+        if(test[4]=='1')
+        {
+            wheel = wheel + 1000;
+        }
+      
+    }
+
+    else if(test[0]=='b')
+    {}
+    else {;}
+
+
+    //printf("%d\n",wheel);
+    fclose(f);    
+    return wheel;
+}
+
+
 char* make_command(char *id,char *device,char *data)
 {
     static char str[100];
     static char *test;
-    char *cmd="../can-utils/cansend can0 ";
+    char *cmd="~/Desktop/sdcard1/cansource/can-utils/cansend can0 ";// 이건 나중에 수정 cansend 위치
     
     strcpy(str,cmd);
     strcat(str,id);
@@ -26,19 +66,45 @@ char* make_command(char *id,char *device,char *data)
 int main()
 {
     FILE *fp;
-    int state;
+    char *canmessage;
+    int wheeldata=0; 
+    char *id,*dev;
+    int data=0;
+    char hex[2];
     int i=0;
-    char buff[MAXLINE];
-    char *test;
-   
-    test=make_command("7df","1","22");
-    printf("%s",test);
+    char *newdata;
+    int state;
 
-    fp = popen(test, "r");
+
+    while(1){
+    wheeldata = Get_wheel_value();
+    id="7df";
+    if(wheeldata<=1000)
+    {
+        dev="0";data=wheeldata;
+    }
+    else if(wheeldata>=1000)
+    {
+        dev="1";data=wheeldata-1000;
+    }
+
+    hex[0]=(data/16);hex[1]=(data%16);
+    
+    if(hex[0]<=9){hex[0]=hex[0]+48;}
+    else if(hex[0]>9){hex[0]=hex[0]+87;}
+    if(hex[1]<=9){hex[1]=hex[1]+48;}
+    else if(hex[1]>9){hex[1]=hex[1]+87;}
+    newdata=hex;
+    canmessage=make_command(id,dev,newdata);
+
+    fp = popen(canmessage, "r");
     if (fp == NULL)
     {
         perror("erro : ");
         exit(0);
+    }
+    state=fclose(fp);
+    
     }
 
 }
