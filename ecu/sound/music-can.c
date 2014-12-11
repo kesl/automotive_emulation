4b2959f4e1b1e30b49ee5ffd35de6c246190b9be
mijong <emarnsndiej@naver.com>
2014-12-11 07:26:42 +0000

    fix sound source

--- ecu/sound/music-can.c
+++ ecu/sound/music-can.c
@@ -2,28 +2,33 @@
 #include <string.h>
 
 #define MAXLINE 256
-unsigned int musicNumber=1;
+int musicNumber=1;
+int status=0;
+
 
 void audioPlayer(char CMD)
 {
 
     static char str[100];
     char *aplay ="sudo aplay ";
-    char *listOfMusic[]={"alam.wav","reme.wav"};
+    char *listOfMusic[]={"bro.wav","inno.wav","park.wav","you.wav"};
     strcpy(str,aplay);
     strcat(str,listOfMusic[musicNumber]);
      
         switch (CMD)
         {
-            case '6':   popen(str,"w");
+            case '6':   if(status==0)
+                        {popen(str,"w");status=1;}
                         break;
             
-            case '7' :  popen("sudo killall aplay","w");
+            case '7' :  if(status==1)
+                        {popen("sudo killall aplay","w");status=0;}
                         break;
             
             case '8' :  musicNumber--;
                         if(musicNumber>=0){}
                         else if(musicNumber<0){musicNumber=0;}
+
                         break;
             
             case '9' :  musicNumber++;
@@ -50,7 +55,7 @@ int main()
     char bytelength=0;
     char *data;    
 
-    fp = popen("./can-utils/candump can0", "r");
+    fp = popen("candump can0", "r");
     
     if (fp == NULL)
     {
@@ -60,9 +65,10 @@ int main()
 
     while(fgets(buff, MAXLINE, fp) != NULL)
     {
-        fgets(buff, MAXLINE, fp);
         data=buff;
         audioPlayer(data[19]);
+        //if(data[19]=='8'||data[19]=='9'){audioPlayer('7');audioPlayer('6');}
+
     }
 
     state = pclose(fp);
