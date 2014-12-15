d168630900363161b45c0e285dafaeee61ca23a2
mijong <emarnsndiej@naver.com>
2014-12-15 04:58:35 +0000

    fix music can source

--- ecu/sound/music-can.c
+++ ecu/sound/music-can.c
@@ -10,8 +10,8 @@ void audioPlayer(char CMD)
 {
 
     static char str[100];
-    char *aplay ="sudo aplay ";
-    char *listOfMusic[]={"bro.wav","inno.wav","park.wav","you.wav"};
+    char *aplay ="sudo aplay /home/pi/automotive_emulation/ecu/sound";
+    char *listOfMusic[]={"kiki.wav","inno.wav","you.wav"};
     strcpy(str,aplay);
     strcat(str,listOfMusic[musicNumber]);
      
